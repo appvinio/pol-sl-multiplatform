2e3d7cc060097042005bbda554ab049aaf76106c
rafaldziuryk <rafal.dziuryk@appvinio.com>
2021-11-16 23:39:35 +0100

    clean repo

--- lib/features/weather/data/datasources/open_weather_datasource.dart
+++ lib/features/weather/data/datasources/open_weather_datasource.dart
@@ -1,7 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:pol_sl_wieloplatformowosc/core/app/api_keys.dart';
 import 'package:pol_sl_wieloplatformowosc/features/weather/data/models/weather_response_model.dart';
-
 
 abstract class OpenWeatherDataSource {
   Future<WeatherResponseModel> getCurrentWeatherData(String cityName);
@@ -15,7 +13,7 @@ class OpenWeatherDataSourceImpl implements OpenWeatherDataSource {
   @override
   Future<WeatherResponseModel> getCurrentWeatherData(String cityName) async {
     final result = await dio.get('',
-        queryParameters: {'q': '$cityName', 'appid': ApiKeys.openWeatherApiKey, 'units': 'metric', 'lang': 'pl'});
+        queryParameters: {'q': '$cityName', 'appid': 'c71343e31d7f9b95579f2f286383db34', 'units': 'metric', 'lang': 'pl'});
     return WeatherResponseModel.fromJson(result.data);
   }
 }
